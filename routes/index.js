8eae7d586b1e7714372cc115ca5d8dcfa0493935
Eric Ritchey <eric@eandjdesign.com>
2017-05-18 23:24:16 -0700

    removing reverse lookup url

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,6 @@ const storeController = require('../controllers/storeController');
 // Do work here
 router.get('/',storeController.homePage);
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [ ...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+
 
 module.exports = router;
