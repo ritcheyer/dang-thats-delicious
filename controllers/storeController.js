486184abad9551b97721467f6a53ea2ec09a41c8
Eric Ritchey <eric@eandjdesign.com>
2017-05-18 23:42:22 -0700

    learning about middleware

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,8 @@
+exports.myMiddleware = (req, res, next) => {
+    req.name = 'Eric';
+    next();
+}
 exports.homePage = (req, res) => {
+    console.log(req.name);
     res.render('index');
 }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
 const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/',storeController.homePage);
+router.get('/', storeController.myMiddleware, storeController.homePage);
 
 
 
