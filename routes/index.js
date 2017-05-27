4d88a4356a4d596b16118a32e8f2f0f15939d029
Eric Ritchey <eric@eandjdesign.com>
2017-05-26 22:04:47 -0700

    learning how to use async await... pretty cool stuff

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
+const { catchErrors } = require('../handlers/errorhandlers');
+
 // Do work here
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
